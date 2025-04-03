52b45498dc5490796343d419156250623edb5022
Hamervit <harold.mercado@sinco.com.co>
2025-04-03 11:14:39 -0500

    First commit with one test

---
+++ README.md
@@ -0,0 +1,12 @@
+# KATA
+https://sammancoaching.org/kata_descriptions/rpg_combat.html
+
+## Test list
+- [ ] Debe permitir crear un personaje con 1000 puntos de vida y en estado Vivo.
+- [ ] Debe tener dos estados posibles Vivo y muerto.
+- [ ] Debe un personaje infligir daño a otro personaje y reducir su vida en la cantidad del daño.
+- [ ] Debe cambiar el estado de un personaje a muerto cuando su vida es igual o menor a 0.
+- [ ] No debe poder infligirse daño a sí mismo.
+- [ ] Debe poder curarse a sí mismo si está Vivo.
+- [ ] No debe poder curarse si está Muerto.
+- [ ] Debe aumentar su vida en la cantidad curada al curarse a sí mismo.
---
+++ RPGCombat.Domain.Tests/CharacterSpecification.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace RPGCombat.Domain.Tests
+{
+    public class CharacterSpecification
+    {
+        [Fact]
+        public void Debe_permitir_crear_un_personaje_con_1000_puntos_de_vida_y_en_estado_vivo()
+        {
+            // Arrange
+            var character = new Character();
+
+            // Act
+            var health = character.Health;
+            var isAlive = character.IsAlive;
+
+            // Assert
+            health.Should().Be(1_000);
+            isAlive.Should().BeTrue();
+        }
+    }
+
+    public class Character
+    {
+        public int Health { get; private set; }
+        public bool IsAlive { get; private set; }
+
+        public Character()
+        {
+            Health = 1_000;
+            IsAlive = true;
+        }
+    }
+}

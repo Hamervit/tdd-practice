4628f31fe2f2d4874f502449b8ab65660c16cba0
Hamervit <harold.mercado@sinco.com.co>
2025-04-04 11:14:30 -0500

    Tests damage and health old Cosmos rules

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # KATA
 https://sammancoaching.org/kata_descriptions/rpg_combat.html
 
+## Definiciones
+### Daño y salud
+* Todos los personajes son creados con 1000 de vida y vivos. Pueden estar vivos o muertos.
+* Los personajes pueden realizar daño a otros
+* Cuando un personaje recibe daño y su vida llega a 0, el personaje muere.
+* Un personaje no debe hacerse daño a si mismo.
+* Un personaje puede curarse a si mismo.
+* Los personajes muertos no pueden curarse.
+
+### Niveles
+* Todos los personajes empiezan en el nivel 1.
+* La vida máxima de los personajes es de 1000, una vez alcacen el nivel 6 su vida máxima incrementa a 1500.
+* Si el objetivo del ataque de un personaje es otro 5 o más niveles por encima, el daño del ataque se reduce en un 50%.
+* Si el objetivo del ataque de un personaje es otro personaje 5 o más niveles por debajo, el daño del ataque aumenta en un 50%.
+
 ## Test list
-- [ ] Debe permitir crear un personaje con 1000 puntos de vida y en estado Vivo.
-- [ ] Debe tener dos estados posibles Vivo y muerto.
-- [ ] Debe un personaje infligir daño a otro personaje y reducir su vida en la cantidad del daño.
-- [ ] Debe cambiar el estado de un personaje a muerto cuando su vida es igual o menor a 0.
-- [ ] No debe poder infligirse daño a sí mismo.
-- [ ] Debe poder curarse a sí mismo si está Vivo.
-- [ ] No debe poder curarse si está Muerto.
-- [ ] Debe aumentar su vida en la cantidad curada al curarse a sí mismo.
+- [X] Debe permitir crear un personaje con 1000 puntos de vida y en estado vivo.
+- [X] Debe un personaje infligir daño a otro personaje y reducir su vida en la cantidad del daño.
+- [X] Debe un personaje morir si recibe un ataque mayor a su vida actual.
+- [X] Debe cambiar el estado de un personaje a muerto cuando su vida es igual o menor a 0.
+- [X] Debe evitar que un personaje se haga daño a si mismo.
+- [X] Debe poder curarse a sí mismo si está vivo.
+- [X] Debe arrojar una excepcion si un personaje muerto intenta curarse.
+- [X] Debe aumentar su vida en la cantidad curada al curarse a sí mismo.
+---------------------------------------------
+
--- RPGCombat.Domain.Tests/CharacterSpecification.cs
+++ RPGCombat.Domain.Tests/CharacterSpecification.cs
@@ -12,23 +12,155 @@ namespace RPGCombat.Domain.Tests
 
             // Act
             var health = character.Health;
-            var isAlive = character.IsAlive;
+            var isAlive = character.IsAlive();
 
             // Assert
             health.Should().Be(1_000);
             isAlive.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(100, 900), InlineData(800, 200)]
+        public void Debe_un_personaje_infligir_daño_a_otro_personaje_y_reducir_su_vida_en_la_cantidad_del_daño(int damage, int defenderHealth)
+        {
+            // Arrange
+            var attacker = new Character();
+            var defender = new Character();
+
+            // Act
+            attacker.Attack(defender, damage);
+
+            // Assert
+            defender.Health.Should().Be(defenderHealth);
+        }
+
+        [Fact]
+        public void Debe_un_personaje_morir_si_recibe_un_ataque_mayor_a_su_vida_actual()
+        {
+            // Arrange
+            var attacker = new Character();
+            var defender = new Character();
+
+            // Act
+            attacker.Attack(defender, 1_001);
+
+            // Assert
+            defender.Health.Should().BeLessThanOrEqualTo(0);
+        }
+
+        [Fact]
+        public void Debe_cambiar_el_estado_de_un_personaje_a_muerto_cuando_su_vida_es_igual_o_menor_a_0()
+        {
+            // Arrange
+            var attacker = new Character();
+            var defender = new Character();
+
+            attacker.Attack(defender, 1_000);
+
+            // Act
+            var defenderStillAlive = defender.IsAlive();
+
+            // Assert
+            defenderStillAlive.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Debe_evitar_que_un_personaje_se_haga_daño_a_si_mismo()
+        {
+            // Arrange
+            var character = new Character();
+
+            // Act
+            var ataque = () => character.Attack(character, 100);
+
+            // Assert
+            ataque.Should().Throw<InvalidOperationException>()
+                .WithMessage("No puedes hacerte daño a ti mismo.");
+        }
+
+        [Fact]
+        public void Debe_poder_curarse_a_si_mismo_si_esta_vivo()
+        {
+            // Arrange
+            var character = new Character();
+
+            // Act
+            character.Cure(100);
+
+            // Assert
+            character.IsAlive().Should().BeTrue();
+            character.Health.Should().Be(1100);
+        }
+
+        [Fact]
+        public void Debe_arrojar_una_excepcion_si_un_personaje_muerto_intenta_curarse()
+        {
+            // Arrange
+            var character = new Character();
+            character.TakeDamage(1_000);
+
+            // Act
+            var cure = () => character.Cure(1_000);
+
+            // Assert
+            cure.Should().Throw<InvalidOperationException>();
+
+        }
+
+        [Theory]
+        [InlineData(100, 1_100), InlineData(200, 1_200), InlineData(300, 1_300)]
+        public void Debe_aumentar_su_vida_en_la_cantidad_curada_al_curarse_a_si_mismo(int curedHealth, int finalHealth)
+        {
+            // Arrange
+            var character = new Character();
+
+            // Act
+            character.Cure(curedHealth);
+
+            // Assert
+            character.Health.Should().Be(finalHealth);
         }
     }
 
     public class Character
     {
         public int Health { get; private set; }
-        public bool IsAlive { get; private set; }
 
         public Character()
         {
             Health = 1_000;
-            IsAlive = true;
+        }
+
+        public void Attack(Character defender, int damage)
+        {
+            if (this == defender)
+            {
+                throw new InvalidOperationException("No puedes hacerte daño a ti mismo.");
+            }
+
+            defender.TakeDamage(damage);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+
+        public void Cure(int health)
+        {
+            if (IsAlive())
+            {
+                Health += health;
+            }
+            else
+            {
+                throw new InvalidOperationException("No puedes curarte si estás muerto.");
+            }
         }
     }
 }
